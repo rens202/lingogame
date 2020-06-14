8453aa40433d88ac1ec9d6b3fe06e084b1ebd63a
Unknown <rens.dollee@student.hu.nl>
2020-06-14 20:54:37 +0200

    bugfix

--- src/test/java/persistence/GameDaoImplTest.java
+++ src/test/java/persistence/GameDaoImplTest.java
@@ -9,7 +9,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
+
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
--- src/test/java/persistence/WordsDaoImplTest.java
+++ src/test/java/persistence/WordsDaoImplTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-
+import org.mockito.runners.MockitoJUnitRunner;
 import java.sql.ResultSet;
 
 import static org.junit.Assert.*;
